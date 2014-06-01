0af9f8ac67fdccfac1461a588c014de65e17fefb
Frans Slothouber <frans.slothouber@gmail.com>
2014-06-01 21:09:07 +0200

    Cleanup

---
+++ makefile
@@ -0,0 +1,6 @@
+
+
+
+clean :
+	make -C Raw clean
+
--- rf.r
+++
@@ -1,4 +0,0 @@
-library("randomForest", lib.loc="/home/hrishikesh/R/x86_64-pc-linux-gnu-library/2.14")
-train<-read.csv("training.csv")
-weights<-train$Weight
-train$Weight<- 0
