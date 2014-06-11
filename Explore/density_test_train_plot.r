ab9b3ee141b89a84598ce679472a88db06a4d3d5
Frans Slothouber <frans.slothouber@gmail.com>
2014-06-11 11:40:33 +0200

    Density plots

--- Explore/density_test_train_plot.r
+++ Explore/density_test_train_plot.r
@@ -3,7 +3,7 @@
 # This plots the density for the various
 # input variables grouped according to
 #
-#   test / trainingset  and  b /  s
+#   test / trainingset
 #
 #=============================================================================
 
@@ -11,19 +11,13 @@ require(ggplot2)
 
 load(file="../Processed/norm_dataset.rdata")
 
-prediction <- read.table(
-    "../Predictions/xgboost_120.csv", sep=",", header=TRUE)
-
-dataset[dataset$test,"Label"] <- prediction$Class
-dataset$foo <- interaction(dataset$Label, dataset$test)
-
 factors <- labels(dataset)[2][[1]]
 factors <- factors[grep("PRI|DER", factors)]
 
 pdf("density_plot.pdf")
 for (f in factors) {
     p1 <- ggplot(dataset[,c(f,"test","Label")], aes_string(x=f)) + 
-        geom_density(aes(color=test))+facet_grid(~Label)
+        geom_density(aes(color=test))
     print(p1)
 }
 dev.off()
